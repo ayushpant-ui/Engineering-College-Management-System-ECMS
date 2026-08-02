67a4758805dd43c1f52bd9a40ced00865365483d
ayushpant-ui <ayushpant2073@gmail.com>
2026-08-03 01:42:40 +0545

    Working on Login Section(FrontEnd)

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,26 @@
+import LoginCard from "@/components/login/LoginCard";
+import LoginIllustration from "@/components/login/LoginIllustration";
+
+export default function LoginPage() {
+  return (
+    <main className="min-h-screen bg-background">
+
+      <section className="mx-auto flex min-h-screen max-w-7xl">
+
+        {/* Left Side */}
+        <div className="hidden lg:flex lg:w-1/2">
+          <LoginIllustration />
+        </div>
+
+        {/* Right Side */}
+        <div className="flex w-full items-center justify-center px-6 py-12 lg:w-1/2">
+
+          <LoginCard />
+
+        </div>
+
+      </section>
+
+    </main>
+  );
+}
