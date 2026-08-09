b62fc4b43902d0e2c235e56a006b48be8a77ab48
ayushpant-ui <ayushpant2073@gmail.com>
2026-08-09 12:59:10 +0545

    Working on Contact Section (Backend)

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ControllerService } from './controller/controller.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [ControllerService, UsersService],
+  providers: [UsersService],
   controllers: [UsersController]
 })
 export class UsersModule {}
